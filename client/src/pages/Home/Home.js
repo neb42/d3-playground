ca15f1c94d1345979ed7a07d9012ef39f3acf57a
neb42 <bmcalindin@gmail.com>
2018-06-01 13:38:32 +0100

    Add some styling tocircle tooltip

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PALETTE } from '@asidatascience/adler-ui';
 
 const pages = [
-  { url: '/bar-chart', name: 'Bar Chart' },
-  { url: '/nyc-crime', name: 'NYC Crime' },
-  { url: '/crypto', name: 'Crypto' },
+  { url: '/bar-chart', name: 'Bar Chart', description: 'A animated bar chart with random data' },
+  { url: '/nyc-crime', name: 'NYC Crime', description: 'A multi-line chart of New York crimes by borough, normalised by population' },
+  { url: '/crypto', name: 'Crypto', description: 'A line chart of bit coin value with a tooltip showing the average value for the month' },
 ];
 
 const Home = () => (
-  <div>
-    {pages.map(({ url, name }) => (
+  <div style={{
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    marginTop: '200px',
+  }}>
+    {pages.map(({ url, name, description }) => (
       <Link to={url}>
-        <span>{name}</span>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '320px',
+          width: '200px',
+          backgroundColor: PALETTE.grey4,
+          boxShadow: '0 5px 10px 0 rgba(34, 51, 62, 0.08)',
+          alignItems: 'center',
+          textAlign: 'center',
+          textDecoration: 'none',
+          paddingTop: '80px',
+        }}>
+          <span style={{
+            marginBottom: '50px',  
+          }}>
+            {name}
+          </span>
+          <span style={{
+
+          }}>
+            {description}
+          </span>
+        </div>
       </Link>
     ))}
   </div>
